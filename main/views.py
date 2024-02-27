dbada0b28d33fd6777ee79e4c710e12d1595e7f4
Иван Евсеев Романович <73618383+33322111@users.noreply.github.com>
2024-02-27 16:14:02 +0300

    Registered simple tag for categories

--- main/views.py
+++ main/views.py
@@ -4,13 +4,12 @@ from django.shortcuts import render
 from goods.models import Categories
 
 def index(request):
-    categories = Categories.objects.all()
 
     context = {
         'title': "Home - Главная",
         'content': 'Магазин мебели Home',
-        'categories': categories
     }
+
     return render(request, 'main/index.html', context)
 
 
