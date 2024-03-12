95eaf70be8e579d7f223f6606df743bf3c39add7
Иван Евсеев Романович <73618383+33322111@users.noreply.github.com>
2024-03-12 23:40:00 +0300

    Fixed cart view and cart templates

--- users/views.py
+++ users/views.py
@@ -18,7 +18,8 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт!")
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
 
                 return HttpResponseRedirect(reverse('main:index'))
@@ -69,6 +70,7 @@ def profile(request):
 
 def users_cart(request):
     return render(request, 'users/users_cart.html')
+
 @login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта!")
