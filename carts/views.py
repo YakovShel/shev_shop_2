0489f85203e409faf27a0cac3b1c8196bee613c9
Иван Евсеев Романович <73618383+33322111@users.noreply.github.com>
2024-03-14 01:43:51 +0300

    Finished all ajax

--- carts/views.py
+++ carts/views.py
@@ -1,15 +1,14 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-
 from carts.models import Cart
 from carts.utils import get_user_carts
+
 from goods.models import Products
 
 
 def cart_add(request):
-
-    product_id = request.POST.get('product_id')
+    product_id = request.POST.get("product_id")
 
     product = Products.objects.get(id=product_id)
 
@@ -25,7 +24,6 @@ def cart_add(request):
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
     user_cart = get_user_carts(request)
-
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
 
@@ -36,8 +34,29 @@ def cart_add(request):
 
     return JsonResponse(response_data)
 
-def cart_change(request, product_slug):
-    pass
+
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quaantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
+
 
 def cart_remove(request):
     cart_id = request.POST.get("cart_id")
@@ -57,4 +76,3 @@ def cart_remove(request):
     }
 
     return JsonResponse(response_data)
-
