f5da2924aab230be423214f2cd362737e7ae5179
Shel Yakov <yadshel@edu.hse.ru>
2024-04-16 11:49:49 +0300

    fix and add html files

--- frontend/js/registration.js
+++ frontend/js/registration.js
@@ -1,43 +1,30 @@
-document.getElementById('registrationForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // Собираем данные формы
-    const formData = {
-        first_name: document.getElementById('id_first_name').value,
-        last_name: document.getElementById('id_last_name').value,
-        username: document.getElementById('id_username').value,
-        email: document.getElementById('id_email').value,
-        password1: document.getElementById('id_password1').value,
-        password2: document.getElementById('id_password2').value,
-    };
-
-    // Отправляем запрос на регистрацию
-    fetch('/user/registration/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData)
-    })
-    .then(response => {
-        if (!response.ok) {
-            return response.json().then(errorData => {
-                throw new Error(errorData.message);
-            });
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.status === 'success') {
-            console.log('Регистрация успешна:', data.message);
-            // Перенаправление на страницу входа или профиль пользователя
-            window.location.href = 'login.html';
-        } else {
-            alert(data.message); // Вывод сообщения об ошибке
-        }
-    })
-    .catch(error => {
-        console.error('Ошибка регистрации:', error.message);
-        alert(error.message); // Вывод сообщения об ошибке пользователю
+$(document).ready(function() {
+    $('#registration-form').submit(function(event) {
+        event.preventDefault();
+        registerUser();
     });
 });
+
+function registerUser() {
+    var formData = {
+        first_name: $('#id_first_name').val(),
+        last_name: $('#id_last_name').val(),
+        username: $('#id_username').val(),
+        email: $('#id_email').val(),
+        password1: $('#id_password1').val(),
+        password2: $('#id_password2').val(),
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/user/registration/',
+        data: formData,
+        success: function(response) {
+            alert('Регистрация прошла успешно!');
+            window.location.href = '/';
+        },
+        error: function(response) {
+            alert('Ошибка при регистрации. Проверьте введенные данные и попробуйте снова.');
+        }
+    });
+}
