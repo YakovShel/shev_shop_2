1e3a5162ec2d92626df7dadecf0a95bddcddd574
Иван Евсеев Романович <73618383+33322111@users.noreply.github.com>
2024-04-17 00:42:34 +0300

    Fixed main API

--- main/urls.py
+++ main/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.urls import path
 
 from main import views
+from main.views import AuthCheckAPIView, AboutAPIView
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', AuthCheckAPIView.as_view(), name='index'),
+    path('about/', AboutAPIView.as_view(), name='about'),
 ]
--- shev/settings.py
+++ shev/settings.py
@@ -62,12 +62,20 @@ MIDDLEWARE = [
 
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware'
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = 'shev.urls'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 TEMPLATES = [
     {
